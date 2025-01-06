85f1e5836568667254cd18349d9c567f98c34a13
Madina-Web-Developer <developermadina761@gmail.com>
2025-01-06 17:30:07 +0600

    responside add korse lg device a

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
 import Banana from '../../assets/Banana.png'
 import Strawberry from '../../assets/Strawberry.png'
@@ -6,9 +6,18 @@ import Kiwi from '../../assets/Kiwi.png'
 import Bluberry from '../../assets/Bluberry.png'
 
 const Banner = () => {
+
+    //    Customes Variables 
+
+
+    const [foodrotate, setfoodrotate] = useState('rotate-[0deg]')
+
+    const [bgcolor, setbgcolor] = useState({background:'bg-[#fff7cf]' , cardbg:'bg-[#F5DE6E]'})
+
+
   return (
     <div>
-        <div className="all_mina_Banner relative overflow-hidden">
+        <div className={`all_mina_Banner relative transition-all duration-[1s] overflow-hidden ${bgcolor.background}`}>
             <div className="container">
 
            
@@ -23,17 +32,16 @@ const Banner = () => {
                     <p>We're dedicated to bringing you deliciously fresh smoothies, made with love, care, and the finest ingredients.</p>
 
                     <div className="buttons_bati">
-                        <button className='Clic' ><img src={Banana} alt="btn_pic" /></button>
-                        <button className='Clic' ><img src={Strawberry} alt="btn_pic" /></button>
-                        <button className='Clic' ><img src={Kiwi} alt="btn_pic" /></button>
-                        <button className='Click' ><img src={Bluberry} alt="btn_pic" /></button>
-                       
-                    </div>
+                        <button onClick={()=>{setfoodrotate('rotate-[0deg]'),setbgcolor((prev)=>({...prev, background:'bg-[#fff7cf]', cardbg:'bg-[#F5DE6E]'}))}} className='Clic' ><img src={Banana} alt="btn_pic" /></button>
+                        <button onClick={()=>{setfoodrotate('rotate-[55deg]'), setbgcolor((prev)=>({...prev, background:'bg-[#FFE1E2]', cardbg:'bg-[#FCB1B4]'}))}} className='Clic' ><img src={Strawberry} alt="btn_pic" /></button>
+                        <button onClick={()=>{setfoodrotate('rotate-[150deg]'),setbgcolor((prev)=>({...prev, background:'bg-[#ECFFCC]', cardbg:'bg-[#8FB351]'})) }} className='Clic' ><img src={Kiwi} alt="btn_pic" /></button>
+                        <button onClick={()=>{setfoodrotate('rotate-[233deg]'), setbgcolor((prev)=>({...prev, background:'bg-[#FFCBEA]', cardbg:'bg-[#C976A7]'}))}} className='Click' ><img src={Bluberry} alt="btn_pic" /></button>
+                     </div>
 
                 </div>
 
                 <div className="animation_part">
-                    <div className="cercel_div absolute">
+                    <div className={`cercel_div ${bgcolor.cardbg}  ${foodrotate}`}>
                         <div className="bati bati_1"><img src={Banana} alt="bati_img" /></div>
                         <div className="bati bati_2"><img src={Strawberry} alt="bati_img" /></div>
                         <div className="bati bati_3"><img src={Kiwi} alt="bati_img" /></div>
@@ -50,3 +58,12 @@ const Banner = () => {
 }
 
 export default Banner
+
+
+//  Banana rotate-[0deg]
+
+// Strawberry rotate-[55deg]
+
+// Kiwi rotate-[150deg]
+
+// Bluberry rotate-[233deg]
